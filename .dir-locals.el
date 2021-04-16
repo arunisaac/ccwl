34b020eecc80157677d18350f8e7cc1f115c330a
Arun Isaac <arunisaac@systemreboot.net>
2021-04-16 23:19:15 +0530

    Add .dir-locals.el for emacs.
    * .dir-locals.el: New file.

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil
+  (indent-tabs-mode))
+ (scheme-mode
+  (eval put 'set-input-source 'scheme-indent-function nil)
+  (eval put 'set-step-in 'scheme-indent-function nil)
+  (eval put 'lambda** 'scheme-indent-function 1)))
