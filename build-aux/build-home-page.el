3d3f90d66f801e9ac0e25e32992604c2be1d2750
Arun Isaac <arunisaac@systemreboot.net>
2021-05-24 18:07:35 +0530

    Build website from README.org.
    * Makefile.am (website, website/index.html, clean-local): New targets.
    * build-aux/build-home-page.el: New file.

---
+++ build-aux/build-home-page.el
@@ -0,0 +1,37 @@
+;;; ccwl --- Concise Common Workflow Language
+;;; Copyright © 2021 Arun Isaac <arunisaac@systemreboot.net>
+;;;
+;;; This file is part of ccwl.
+;;;
+;;; ccwl is free software: you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; ccwl is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with ccwl.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This Emacs script generates the home page of the website from
+;; README.org.
+
+;;; Code:
+
+(require 'ox-html)
+
+(setq org-export-with-section-numbers nil
+      org-export-with-sub-superscripts nil
+      org-export-with-toc nil
+      org-html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\" />"
+      org-html-head-include-default-style nil
+      org-html-head-include-scripts nil
+      org-html-postamble nil)
+
+(with-current-buffer (find-file "README.org")
+  (org-export-to-file 'html "website/index.html"))
