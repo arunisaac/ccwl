5dfe6c32cb769b31bd85ec83b21c411088ba9167
Arun Isaac <arunisaac@systemreboot.net>
2021-07-02 18:03:48 +0530

    .dir.locals.el: Remove unused indentation specifiers.
    * .dir-locals.el (scheme-mode): Remove indentation specification for
    set-input-source, set-output-source and set-step-in.

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,8 +4,5 @@
 ((nil
   (indent-tabs-mode))
  (scheme-mode
-  (eval put 'set-input-source 'scheme-indent-function nil)
-  (eval put 'set-output-source 'scheme-indent-function nil)
-  (eval put 'set-step-in 'scheme-indent-function nil)
   (eval put 'lambda** 'scheme-indent-function 1)
   (eval put 'workflow 'scheme-indent-function 1)))
