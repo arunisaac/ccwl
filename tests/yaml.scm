993441fc39580d4c47235a37a9c47a67cc0bbfc9
Arun Isaac <arunisaac@systemreboot.net>
2021-07-19 15:50:10 +0530

    tests: Fix order of arguments to test-equal.
    The expected value should be the first argument to test-equal. Getting
    this order wrong messes up the test logs.
    
    * tests/yaml.scm ("dictionary entries with empty arrays and
    dictionaries for values must render on the same line"): Fix order of
    arguments to test-equal.

--- tests/yaml.scm
+++ tests/yaml.scm
@@ -22,12 +22,12 @@
 (test-begin "yaml")
 
 (test-equal "dictionary entries with empty arrays and dictionaries for values must render on the same line"
-  (scm->yaml-string
-   '((foo . #())
-     (bar)))
   "foo: []
 bar: {}
-")
+"
+  (scm->yaml-string
+   '((foo . #())
+     (bar))))
 
 (test-equal "strings with hyphen characters should be escaped"
   "- \"-1\"
