781e504bcc50bee661cecd55377b55ab2834e01d
Arun Isaac <arunisaac@systemreboot.net>
2021-12-10 15:48:36 +0530

    guix.scm: Base ccwl package off upstream Guix package.
    * guix.scm: Do not import (gnu packages graphviz), (gnu packages
    guile), (gnu packages guile-xyz), (gnu packages pkg-config), (gnu
    packages skribilo), (gnu packages texinfo), (guix build-system
    gnu), (guix licenses) and (guix utils). Import (gnu packages
    bioinformatics) with the guix: prefix.
    (ccwl): Base off upstream Guix package.

--- guix.scm
+++ guix.scm
@@ -28,64 +28,22 @@
 ;;; Code:
 
 (use-modules (gnu packages autotools)
-             (gnu packages bioinformatics)
-             (gnu packages graphviz)
-             (gnu packages guile)
-             (gnu packages guile-xyz)
-             (gnu packages pkg-config)
-             (gnu packages skribilo)
-             (gnu packages texinfo)
-             (guix build-system gnu)
+             ((gnu packages bioinformatics) #:prefix guix:)
              (guix gexp)
              (guix git-download)
-             (guix packages)
-             ((guix licenses) #:prefix license:)
-             (guix utils))
+             (guix packages))
 
 (define %source-dir (dirname (current-filename)))
 
 (define ccwl
   (package
-    (name "ccwl")
-    (version "0.2.0")
+    (inherit guix:ccwl)
     (source (local-file %source-dir
                         #:recursive? #t
                         #:select? (git-predicate %source-dir)))
-    (build-system gnu-build-system)
-    (arguments
-     `(#:make-flags '("GUILE_AUTO_COMPILE=0") ; to prevent guild warnings
-       #:modules (((guix build guile-build-system)
-                   #:select (target-guile-effective-version))
-                  ,@%gnu-build-system-modules)
-       #:imported-modules ((guix build guile-build-system)
-                           ,@%gnu-build-system-modules)
-       #:phases
-       (modify-phases %standard-phases
-         (add-after 'install 'wrap
-           (lambda* (#:key inputs outputs #:allow-other-keys)
-             (let ((out (assoc-ref outputs "out"))
-                   (effective-version (target-guile-effective-version)))
-               (wrap-program (string-append out "/bin/ccwl")
-                 `("GUILE_LOAD_PATH" prefix
-                   (,(string-append out "/share/guile/site/" effective-version)
-                    ,(getenv "GUILE_LOAD_PATH")))
-                 `("GUILE_LOAD_COMPILED_PATH" prefix
-                   (,(string-append out "/lib/guile/" effective-version "/site-ccache")
-                    ,(getenv "GUILE_LOAD_COMPILED_PATH"))))))))))
-    (inputs
-     `(("guile" ,guile-3.0)
-       ("guile-libyaml" ,guile-libyaml)))
     (native-inputs
      `(("autoconf" ,autoconf)
        ("automake" ,automake)
-       ("pkg-config" ,pkg-config)
-       ;; To build documentation
-       ("cwltool" ,cwltool)
-       ("graphviz" ,graphviz)
-       ("skribilo" ,skribilo)))
-    (home-page "https://git.systemreboot.net/ccwl")
-    (synopsis "Concise common workflow language")
-    (description "Concise common workflow language")
-    (license license:gpl3+)))
+       ,@(package-native-inputs guix:ccwl)))))
 
 ccwl
