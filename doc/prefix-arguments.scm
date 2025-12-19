191fffe866f720d83b65dcc9351c50d83ca9a5b1
Arun Isaac <arunisaac@systemreboot.net>
2025-12-19 20:09:34 +0000

    doc: Do not capture output in prefix arguments example.
    The output capture distracts from the main message.

--- doc/prefix-arguments.scm
+++ doc/prefix-arguments.scm
@@ -1,5 +1,2 @@
 (command #:inputs (source #:type File) (output_filename #:type string)
-         #:run "gcc" source ("-o" output_filename)
-         #:outputs (executable
-                    #:type File
-                    #:binding ((glob . "$(inputs.output_filename)"))))
+         #:run "gcc" source ("-o" output_filename))
